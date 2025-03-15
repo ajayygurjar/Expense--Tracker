ab4b312348e385fa5836918882dd63d898f9e906
ajayygurjar <ajay1651@gmail.com>
2025-03-16 01:13:10 +0530

    Auth login and error message

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Input from "../UI/Input";
+import Home from "../../Pages/Home";
 
 const AuthForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const [errorMessage, setErrorMessage] = useState("");
   const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   const API_KEY = `AIzaSyDmSv7uTvH1Dsz9pWQEa9-BztI1xV9F4H0`;
   const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+  const SIGNIN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
 
   const showErrorMessage = (message) => {
     setErrorMessage(message);
@@ -21,50 +24,73 @@ const AuthForm = () => {
 
   const authFormHandler = async (event) => {
     event.preventDefault();
-
-    if (password !== confirmPassword) {
-      showErrorMessage("Passwords don't match");
-      return;
-    }
-
     const userAuthData = {
       email: email,
       password: password,
       returnSecureToken: true,
     };
 
-    console.log(userAuthData);
-
-    try {
-      if (password === confirmPassword) {
-        const response = await axios.post(SIGNUP_URL, userAuthData);
-        console.log(response.data);
-      } else {
-        setErrorMessage("Password doesnt Match");
-        setIsErrorVisible(true);
-        setTimeout(() => setIsErrorVisible(false), 3000);
+    if (isSignUp) {
+      if (password !== confirmPassword) {
+        showErrorMessage(`Password don't match`);
+        return;
       }
-    } catch (error) {
-      if (error.response.data.error.message === "EMAIL_EXISTS") {
-        setErrorMessage("*Email is Already Registered");
-        setIsErrorVisible(true);
-      } else if (
-        error.response.data.error.message ===
-        "WEAK_PASSWORD : Password should be at least 6 characters"
-      ) {
-        setErrorMessage(
-          "*Weak Password, Password should be at least 6 characters"
-        );
-        setIsErrorVisible(true);
-      }
-      setTimeout(() => setIsErrorVisible(false), 3000);
     }
+
+    if (!isSignUp) {
+      try {
+        const response = await axios.post(SIGNIN_URL, userAuthData);
+        console.log(response.data);
+      } catch (error) {
+        if (error.response && error.response.data.error.message) {
+          const errorMsg = error.response.data.error.message;
+          if (errorMsg === "EMAIL_NOT_FOUND") {
+            showErrorMessage("*Email not found");
+          } else if (errorMsg === "INVALID_LOGIN_CREDENTIALS") {
+            showErrorMessage("*Invalid password");
+          } else {
+            showErrorMessage("An unexpected error occurred");
+          }
+        }
+      }
+    } else {
+      try {
+        if (password === confirmPassword) {
+          const response = await axios.post(SIGNUP_URL, userAuthData);
+
+          console.log(response.data);
+        } else {
+          setErrorMessage("* Password doesnt Match");
+          setIsErrorVisible(true);
+          setTimeout(() => setIsErrorVisible(false), 3000);
+        }
+      } catch (error) {
+        if (error.response.data.error.message === "INVALID_EMAIL") {
+          setErrorMessage("* Enter a Valid Email");
+        } else if (error.response.data.error.message === "EMAIL_EXISTS") {
+          setErrorMessage("* Email is Already Registered");
+        } else if (
+          error.response.data.error.message ===
+          "WEAK_PASSWORD : Password should be at least 6 characters"
+        ) {
+          setErrorMessage("* Password should be at least 6 characters");
+        }
+      }
+    }
+  };
+
+  const toggleSignUpLoginHandler = () => {
+    setIsSignUp((prev) => !prev);
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setErrorMessage("");
   };
 
   return (
     <main>
       <section>
-        <h1>SignUp</h1>
+        <h1>{isSignUp ? `Sign Up` : `Login`}</h1>
         <form onSubmit={authFormHandler}>
           <div>
             <div>
@@ -89,25 +115,31 @@ const AuthForm = () => {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div>
-              <Input
-                label="Confirm Password"
-                id="confirmPassword"
-                name="confirmPassword"
-                type="password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
+            {isSignUp && (
+              <div>
+                <Input
+                  label="Confirm Password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+            )}
           </div>
           {isErrorVisible && <p style={{ color: "red" }}>{errorMessage}</p>}
           <div>
-            <button type="submit">Sign Up</button>
+            <button type="submit">{isSignUp ? `Sign Up` : `Login`}</button>
           </div>
         </form>
       </section>
       <div>
-        <button type="button">Have an account? Login</button>
+        <button type="button" onClick={toggleSignUpLoginHandler}>
+          {isSignUp
+            ? `Have an account  ? Login`
+            : `Don't Have an account ? Sign Up`}
+        </button>
       </div>
     </main>
   );
